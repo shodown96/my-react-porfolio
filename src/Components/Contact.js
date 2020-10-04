d4daff144615dc6de46c2187934387b74d270861
Elijah Soladoye <shodown96@gmail.com>
2020-10-04 21:41:14 +0100

    hmn

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -7,6 +7,14 @@ const Contact = ({ data }) => {
    // const [email, setEmail] = useState('');
    // const [message, setMessage] = useState('');
 
+   // const [formData, setFormData] = useState({
+   //    name:"",
+   //    email:"",
+   //    subject:"",
+   //    message:"",
+
+   // })
+
    // console.log(data)
 
    //  const handleClick = (e) => {
@@ -15,8 +23,23 @@ const Contact = ({ data }) => {
    //    alert("Accepted")
    //    document.querySelector("#message-success").style.display = "block";
    //  }
-   
-   // const handleSubmit = e => {e.preventDefault();}
+   // const encode = (data) => {
+   //    return Object.keys(data)
+   //        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+   //        .join("&");
+   //  }
+
+   // const handleSubmit = e => {
+   //    e.preventDefault();
+   //    // fetch("/", {
+   //    //    method: "POST",
+   //    //    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+   //    //    body: encode({ "contactForm": "contact", ...this.state })
+   //    //  })
+   //    //    .then(() => alert("Success!"))
+   //    //    .catch(error => alert(error));
+   //    console.log(formData)
+   // }
 
     return (
       <section id="contact">
@@ -43,25 +66,25 @@ const Contact = ({ data }) => {
                <form id="contactForm" name="contactForm" method="post">
 
 					<fieldset>
-                  <input type="hidden" name="form-name" value="contactForm" />
+                  {/* <input type="hidden" name="contactForm" value="contactForm" /> */}
                   <div>
-						   <label htmlFor="contactName">Name <span className="required">*</span></label>
-						   <input type="text" size="35" id="contactName" name="contactName"/>
+						   <label htmlFor="name">Name <span className="required">*</span></label>
+						   <input type="text" size="35" id="name" name="name"/>
                   </div>
 
                   <div>
-						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-						   <input type="text" size="35" id="contactEmail" name="contactEmail"/>
+						   <label htmlFor="email">Email <span className="required">*</span></label>
+						   <input type="text" size="35" id="email" name="email"/>
                   </div>
 
                   <div>
-						   <label htmlFor="contactSubject">Subject</label>
-						   <input type="text" size="35" id="contactSubject" name="contactSubject"/>
+						   <label htmlFor="subject">Subject</label>
+						   <input type="text" size="35" id="subject" name="subject"/>
                   </div>
 
                   <div>
-                     <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                     <textarea cols="50" rows="5" id="contactMessage" name="contactMessage"></textarea>
+                     <label htmlFor="message">Message <span className="required">*</span></label>
+                     <textarea cols="50" rows="5" id="message" name="message"></textarea>
                   </div>
 
                   <div>
