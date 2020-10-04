466a08368e09fce46fcbb21590e1c81b978bb178
Elijah Soladoye <shodown96@gmail.com>
2020-10-04 21:24:14 +0100

    hmn

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const Contact = ({ data }) => {
    // const [url, setUrl] = useState('mailto:test@example.com?subject=subject&body=body');
-   const [name, setName] = useState('');
-   const [subject, setSubject] = useState('');
-   const [email, setEmail] = useState('');
-   const [message, setMessage] = useState('');
+   // const [name, setName] = useState('');
+   // const [subject, setSubject] = useState('');
+   // const [email, setEmail] = useState('');
+   // const [message, setMessage] = useState('');
 
    // console.log(data)
 
@@ -16,7 +16,7 @@ const Contact = ({ data }) => {
    //    document.querySelector("#message-success").style.display = "block";
    //  }
    
-   const handleSubmit = e => {e.preventDefault();}
+   // const handleSubmit = e => {e.preventDefault();}
 
     return (
       <section id="contact">
@@ -40,28 +40,28 @@ const Contact = ({ data }) => {
          <div className="row">
             <div className="eight columns">
 
-               <form id="contactForm" name="contactForm" method="post" onSubmit={handleSubmit}>
+               <form id="contactForm" name="contactForm" method="post">
 
 					<fieldset>
                   <input type="hidden" name="form-name" value="contactForm" />
                   <div>
 						   <label htmlFor="contactName">Name <span className="required">*</span></label>
-						   <input value={name} type="text" size="35" id="contactName" name="contactName" onChange={e => setName(e.target.value)}/>
+						   <input type="text" size="35" id="contactName" name="contactName"/>
                   </div>
 
                   <div>
 						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-						   <input value={email} type="text" size="35" id="contactEmail" name="contactEmail" onChange={e=> setEmail(e.target.value)}/>
+						   <input type="text" size="35" id="contactEmail" name="contactEmail"/>
                   </div>
 
                   <div>
 						   <label htmlFor="contactSubject">Subject</label>
-						   <input value={subject} type="text" size="35" id="contactSubject" name="contactSubject" onChange={e => setSubject(e.target.value)}/>
+						   <input type="text" size="35" id="contactSubject" name="contactSubject"/>
                   </div>
 
                   <div>
                      <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                     <textarea value={message} onChange={e => setMessage(e.target.value)} cols="50" rows="5" id="contactMessage" name="contactMessage"></textarea>
+                     <textarea cols="50" rows="5" id="contactMessage" name="contactMessage"></textarea>
                   </div>
 
                   <div>
