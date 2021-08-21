59be1eff993e68ed9da1e93024806a849fa0f29f
Elijah Soladoye <shodown96@gmail.com>
2021-08-21 22:11:46 +0100

    swiper-removed

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,52 +1,52 @@
 import React, { Component } from 'react';
-import SwiperCore, { EffectCoverflow,Pagination, Autoplay } from 'swiper/core';
+// import SwiperCore, { EffectCoverflow,Pagination, Autoplay } from 'swiper/core';
 // Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
+// import { Swiper, SwiperSlide } from "swiper/react";
 
 // install Swiper modules
-SwiperCore.use([EffectCoverflow,Pagination, Autoplay]);
+// SwiperCore.use([EffectCoverflow,Pagination, Autoplay]);
 
 class Portfolio extends Component {
   render() {
 
-    // if(this.props.data){
-    //   var projects = this.props.data.projects.map(function(projects){
-    //     var projectImage = 'images/portfolio/'+projects.image;
-    //     return <div key={projects.title} className="columns portfolio-item">
-    //        <div className="item-wrap">
-    //         <a target="_blank" rel="noopener noreferrer" href={projects.url} title={projects.title}>
-    //            <img alt={projects.title} src={projectImage} />
-    //            <div className="overlay">
-    //               <div className="portfolio-item-meta">
-    //              <h5>{projects.title}</h5>
-    //                  <p>{projects.category}</p>
-    //               </div>
-    //             </div>
-    //           <div className="link-icon"><i className="fa fa-link"></i></div>
-    //         </a>
-    //       </div>
-    //     </div>
-    //   })
-    // }
-
     if(this.props.data){
       var projects = this.props.data.projects.map(function(projects){
         var projectImage = 'images/portfolio/'+projects.image;
-        return <div className="portfolio-item">
-          <SwiperSlide key={projects.title}> 
-              <a target="_blank" rel="noopener noreferrer" href={projects.url} title={projects.title}>
-              <img src={projectImage} alt=""/>
-              </a>
-              <div className="overlay">
-                <div className="portfolio-item-meta">
-                <h5>{projects.title}</h5>
-                    <p>{projects.category}</p>
+        return <div key={projects.title} className="columns portfolio-item">
+           <div className="item-wrap">
+            <a target="_blank" rel="noopener noreferrer" href={projects.url} title={projects.title}>
+               <img alt={projects.title} src={projectImage} />
+               <div className="overlay">
+                  <div className="portfolio-item-meta">
+                 <h5>{projects.title}</h5>
+                     <p>{projects.category}</p>
+                  </div>
                 </div>
-              </div>
-          </SwiperSlide>
+              <div className="link-icon"><i className="fa fa-link"></i></div>
+            </a>
           </div>
+        </div>
       })
     }
+
+    // if(this.props.data){
+    //   var projects = this.props.data.projects.map(function(projects){
+    //     var projectImage = 'images/portfolio/'+projects.image;
+    //     return <div className="portfolio-item">
+    //       <SwiperSlide className="slide" key={projects.title} style={{backgroundImage:`url(${projectImage})`}}> 
+    //           <a target="_blank" rel="noopener noreferrer" href={projects.url} title={projects.title}>
+    //           <img src={projectImage} alt=""/>
+    //           </a>
+    //           <div className="overlay">
+    //             <div className="portfolio-item-meta">
+    //             <h5>{projects.title}</h5>
+    //                 <p>{projects.category}</p>
+    //             </div>
+    //           </div>
+    //       </SwiperSlide>
+    //       </div>
+    //   })
+    // }
     return (
       <section id="portfolio">
 
@@ -56,12 +56,12 @@ class Portfolio extends Component {
 
             <h1>Hey!! Why Not Check Out Some of My Works.</h1>
 
-            {/* <div id="portfolio-wrapper" className="bgrid-thirds s-bgrid-halves cf">
+            <div id="portfolio-wrapper" className="bgrid-thirds s-bgrid-halves cf">
                 {projects}
-            </div> */}
+            </div>
 
             <div>
-              <Swiper 
+              {/* <Swiper 
                 effect={'coverflow'}
                 grabCursor={true}
                 centeredSlides={true}
@@ -82,7 +82,7 @@ class Portfolio extends Component {
               }}
               pagination={true} className="mySwiper">
                 {projects}
-              </Swiper>
+              </Swiper> */}
 
             </div>
           </div>
